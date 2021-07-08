fd32749327b1ff022eb53a8b57324202454d7df8
Anupam <anupam38b@gmail.com>
2021-07-08 16:22:24 +0530

    update

--- Tree/question.java
+++ Tree/question.java
@@ -33,5 +33,93 @@ public class question{
         
         return ans;
     }
+
+
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        ArrayList<Integer> psf = new ArrayList<>();
+        
+        List<String> ans = new ArrayList<String>();
+        solve(root, psf, ans);
+        return ans;
+    }
+    
+    public void solve(TreeNode root, ArrayList<Integer> psf, List<String> ans){
+        if(root == null){
+            return;
+        }
+        if(root.left == null && root.right == null){
+            psf.add(root.val);
+            String stans = makeans(psf); 
+            ans.add(stans);
+            psf.remove(psf.size()-1);
+        }
+        
+        psf.add(root.val);
+        
+        solve(root.left, psf, ans);
+        solve(root.right, psf, ans);
+        
+        psf.remove(psf.size()-1);
+    } 
+    
+    
+    public String makeans(ArrayList<Integer> arr){
+        String ans = "";
+        for(int i = 0; i < arr.size(); i++){
+            if(i != arr.size()-1) ans = ans + arr.get(i) + "->";
+            else ans = ans + arr.get(i);
+        }
+        return ans;
+    }
+
+
+    // LC-863 All Nodes distance k in binary Tree
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k ){
+        if(root == null) return new ArrayList<>();
+        // if(k == 0) return new ArrayList<>(target.val);
+        
+        ArrayList<TreeNode> arr = new ArrayList<>();
+        nodeToRootpath(root, target, arr);
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeNode blocknode = null;
+        for(int i = 0; i < arr.size() && k-i >= 0; i++){
+            printkdown(arr.get(i) , k-i, ans, blocknode);
+            blocknode = arr.get(i);    
+        }
+        return ans;
+    }
+    
+    public boolean nodeToRootpath(TreeNode root, TreeNode target, ArrayList<TreeNode> psf){     
+        if(root == null) return false;
+        
+        if(root == target){
+            psf.add(root);
+            return true;
+        }
+        
+        boolean ans = false;
+        ans = ans || nodeToRootpath(root.left, target, psf);
+        ans = ans || nodeToRootpath(root.right, target, psf);
+        
+        if(ans){
+            psf.add(root);
+        }
+        return ans;
+    }
+    
+    
+    public void printkdown(TreeNode root, int k, ArrayList<Integer> ans, TreeNode blocknode){
+        if(root == null) return;
+        
+        if(k == 0){
+            ans.add(root.val);
+            return ;
+        }
+        
+        if(blocknode != root.left)  printkdown(root.left, k-1, ans, blocknode);
+        if(blocknode != root.right)  printkdown(root.right, k-1, ans, blocknode);
+    }
     
 }
