da3932e5838b265cba48ad070a95ba9465affceb
Anupam <anupam38b@gmail.com>
2021-07-12 09:58:29 +0530

    update

--- linkedlist/toLearnCode.java
+++ linkedlist/toLearnCode.java
@@ -1,5 +1,27 @@
-// MIDDLE ELEMENT 
+// GET NODE (0 INDEXED) ->  yaha i = 0 se start hoga bass
+public Node getNode(Node head, int idx){
+    Node h1 = head; 
+    for(int i = 0 ; i < idx; i++){
+        h1 = h1.next;
+    }
+    return h1;   // h1 points exactly at idx index of LL
+}
 
+
+// GET NODE (1 INDEXED) ->  yaha i = 1 se start hoga bass
+public Node getNode(Node head, int idx){
+    Node h1 = head;
+    for(int i = 1 ; i < idx; i++){
+        h1 = h1.next;
+    }
+    return h1;   // h1 points exactly at idx index of LL
+}
+
+// =======================================================================================================================
+
+// SLOW-FAST CONCEPT METHOD
+
+// i.  MIDDLE ELEMENT 
 public int middleElement(Node head){
     Node slow = head;
     Node fast = head;
@@ -11,9 +33,24 @@ public int middleElement(Node head){
     return slow;
 }
 
+// ii.  Kth element from last
+public Node kthElementfromLast(Node head, int k){
+    Node slow = head;
+    Node fast = head;
+    
+    for(int i = 0; i < k; i++)  fast = fast.next;      // if (0 indexing) ha LL ki to i = 0 se start hoga,           // ye question me dia hoga ki 1 indexing ha ya 0 indexing ha
+                                                       // if (1 indexing) ha LL ki to i = 1 se start hoga
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
+}
+
+//===================================================================================================================
 
 // REVERSE OF LINKEDLIST
-
 // 1nd way - yahi yaad kar lo sahi rahega (koi null ki condition tension ni)
 public Node reverseLinkedlist(Node head){
     Node prev = null;
@@ -47,26 +84,3 @@ public Node reverseLinkedlist(Node head){
     return prev;
 }
 
-
-// GET NODE (0 INDEXED) ->  yaha i = 0 se start hoga bass
-public Node getNode(Node head, int idx){
-    Node h1 = head; 
-    
-    for(int i = 0; i < idx; i++){
-        h1 = h1.next;
-    }
-
-    return h1;   // h1 point exactly at idx index of the linkedlist
-}
-
-
-// GET NODE (0 INDEXED) ->  yaha i = 1 se start hoga bass
-public Node getNode(Node head, int idx){
-    Node h1 = head;
-
-    for(int i = 1; i < idx; i++){
-        h1 = h1.next;
-    }
-
-    return h1;   // h1 point exactly at idx index of the linkedlist
-}
