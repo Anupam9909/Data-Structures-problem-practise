cd56a10c2a5b63280fb7c37f067f7e5a98d90c16
Anupam <anupam38b@gmail.com>
2021-11-26 09:52:10 +0530

    update

---
+++ searching&sorting/questions.java
@@ -0,0 +1,12 @@
+public class questions{
+
+    public static void main(String[] args){
+
+    }
+}
+
+
+
+
+
+
--- stack/question.java
+++ stack/question.java
@@ -61,9 +61,46 @@ public class question{
 
 
 
+    
+    // lc - 1249. Minimum Remove to Make Valid Parentheses
+    public String minRemoveToMakeValid(String s){
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(ch == ')' || ch == '('){
+                if(ch == ')'){
+                    if(arr.size() == 0) arr.add(i);
+                    else if(s.charAt(arr.get(arr.size()-1)) == '(') arr.remove(arr.size()-1);
+                    else  arr.add(i);
+                }else{
+                    arr.add(i);
+                } 
+            }else {
+                // if an alhabet is present then not to do anything
+            }
+        }
+        
+        // invalid element are present in the stack
+        
+        StringBuilder ans  = new StringBuilder();  // string lege to jada time lagta ha compare to this stringbuilder
+        int j = 0;
+        for(int  i =  0; i < s.length(); i++){
+            if( j < arr.size() && i == arr.get(j)){
+                j++;
+            }else{
+                ans.append(s.charAt(i));
+            }
+        }
+               
+        return ans.toString();
+    }
 
 
-    // remove outermost paranthesis
+
+
+    // LC  - 1021 remove outermost paranthesis
     public String removeOuterMostParam(String str){
         if(str.length() == 0 ) return str;
 
@@ -96,7 +133,73 @@ public class question{
 
 
 
+    // LC - 32. Longest Valid Parentheses
+    public int longestValidParentheses(String s){
+        Stack<Integer> st = new Stack<>();
+        int n = s.length();
+        st.push(-1);
+        
+        int ans = 0;  // length of longest string
+        
+        for(int i = 0 ; i < n; i++){
+            char ch = s.charAt(i);
+            
+            if(st.peek() != -1 && ch == ')' && s.charAt(st.peek()) == '('){
+                st.pop();
+                ans = Math.max(ans, i-st.peek());
+            }else{
+                st.push(i);
+            }
+        }
+        return ans;
+    }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // LC - 735. Asteroid Collision
+    public int[] asteroidCollision(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int n = arr.length;
+        
+        for(int i = 0; i < n; i++){
+            if(arr[i] > 0){
+                st.push(arr[i]);
+            }else{
+                while(st.size()!= 0 && st.peek() > 0 && -arr[i] > st.peek()){
+                    st.pop();
+                }
+                
+                if(st.size() != 0 && st.peek()  == -arr[i]) st.pop();
+                else if(st.size() != 0 && st.peek() > -arr[i]) continue;
+                else st.push(arr[i]);
+            }
+        }
+        
+        int[] ans = new int[st.size()];
+        for(int i = ans.length-1; i >= 0 ; i--){
+            ans[i] = st.pop();
+        }
+        
+        return ans;
+    }
 
 
 
