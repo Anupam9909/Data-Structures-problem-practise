3586f82a37511ddd17cb2e1569e12213185eab09
Anupam <85264125+Anupam9909@users.noreply.github.com>
2026-04-08 03:57:03 +0530

    Refactor RandomizedSet and add RandomizedCollection

--- hashmap/INSERT-DELETE-GETRANDOM.java
+++ hashmap/INSERT-DELETE-GETRANDOM.java
@@ -3,21 +3,18 @@
 class RandomizedSet {
     HashMap<Integer, Integer> hm;
     ArrayList<Integer> arr;
-    int size;
+
     public RandomizedSet() {
         hm = new HashMap<>(); 
         arr = new ArrayList<>();
-        this.size = 0;
     }
     
     public boolean insert(int val) {
         if(hm.containsKey(val)){
             return false;
         }else{
-            int idx = size;
             arr.add(val);
-            hm.put(val, idx);
-            size++;
+            hm.put(val, arr.size()-1);
 
             return true;
         }
@@ -25,24 +22,15 @@ class RandomizedSet {
     
     public boolean remove(int val) {
         if(hm.containsKey(val)){
-            int curridx = hm.get(val);
+            int idx = hm.get(val);
+            int n = arr.size();
+            arr.set(idx, arr.get(n-1));
+            hm.put(arr.get(n-1), idx);
+
+            arr.remove(n-1);
             hm.remove(val);
-            if(curridx == size-1){   // if element is present at last index of arraylist itself
-                arr.remove(size-1);
-                size--;
-                return true;
-            }
             
-            int lastidx = size-1;
-            int lastval = arr.get(lastidx);
-            
-            arr.set(curridx, lastval);
-            arr.remove(size-1);
-            
-            hm.put(lastval, curridx);
-            size--;
-            return true;
-            
+            return true;  
         }else{
             return false;
         }
@@ -50,11 +38,11 @@ class RandomizedSet {
     
     public int getRandom() {
         int min = 0;
-        int max = size-1;
+        int max = arr.size()-1;
         int range = max - min + 1;
-        int rand = (int)(Math.random() * range) + min;  // ye formula ha [to find random value b/w min and max value]
+        int randomIndex = (int)(Math.random() * range) + min;  // ye formula ha [to find random value b/w min and max value]
         
-        return arr.get(rand);
+        return arr.get(randomIndex);
     }
 }
 
@@ -64,3 +52,48 @@ class RandomizedSet {
 
 
 // LC-381  INSERT - DELETE GET RANDOM IN O(1) [when duplicate elements also present]
+class RandomizedCollection {
+    List<Integer> list;
+    Map<Integer, Set<Integer>> map;
+
+    public RandomizedCollection() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+    }
+
+    public boolean insert(int val) {
+        if (!map.containsKey(val)) {
+            map.put(val, new HashSet<>());
+        }
+
+        map.get(val).add(list.size());
+        list.add(val);
+
+        return map.get(val).size() == 1;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val) || map.get(val).isEmpty()) return false;
+
+        int removeIdx = map.get(val).iterator().next();
+        map.get(val).remove(removeIdx);
+
+        int lastElement = list.get(list.size() - 1);
+        list.set(removeIdx, lastElement);
+
+        map.get(lastElement).add(removeIdx);
+        map.get(lastElement).remove(list.size() - 1);
+
+        list.remove(list.size() - 1);
+
+        return true;
+    }
+
+    public int getRandom() {
+        int randomIdx = (int) (Math.random() * list.size());
+        return list.get(randomIdx);
+    }
+}
+
+// TC: O(1) - O(1) - O(1)
+// SC: O(n)
